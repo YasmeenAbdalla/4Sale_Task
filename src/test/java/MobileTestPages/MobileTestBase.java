cbe76a247a4119529b24f5b2ea3ba4a592cb23ee
hp ZBook <yassminabdallah@shgardi.app>
2023-10-12 10:36:41 +0300

    New update in Pom.xml and add allure report

--- src/test/java/MobileTestPages/MobileTestBase.java
+++ src/test/java/MobileTestPages/MobileTestBase.java
@@ -1,18 +1,18 @@
 package MobileTestPages;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MobileTestBase {
-public static AndroidDriver driver;
+public static AppiumDriver driver;
 
 
 
@@ -29,13 +29,14 @@ public static AndroidDriver driver;
 
         AndroidObject.setCapability("automationName", "UiAutomator2");
         AndroidObject.setCapability("app", System.getProperty("user.dir")+"/App/base.apk");
-        driver = new AndroidDriver <WebElement>(new URL("http://localhost:4723/"), AndroidObject);
+        driver = new AndroidDriver (new URL("http://localhost:4723/"), AndroidObject);
 
 
     }
 
-    public void clickToSelectCarType(){
-        driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textContains(\"Audi\").instance(0))").click();
+
+    public WebElement scrollDownIntoText(String text) {
+        return driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textContains(\""+text+"\").instance(0))"));
 
     }
 
